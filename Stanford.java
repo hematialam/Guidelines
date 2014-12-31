4cb5c3c4ca5a5adb759742f20e5d031068fe3e64
Sean Gallagher <stgallag@gmail.com>
2014-12-31 12:19:02 -0500

    Playing tab nanny

--- Stanford.java
+++ Stanford.java
@@ -7,32 +7,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.FileWriter;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
- 
+
 public class tagging {
     public static void main(String[] args) throws IOException,
-            ClassNotFoundException {
+        ClassNotFoundException {
         MaxentTagger tagger = new MaxentTagger(
-                "taggers/bidirectional-distsim-wsj-0-18.tagger");
-        
-        	//reading file line by line in Java using BufferedReader
-        	FileInputStream fis = null;
-        	BufferedReader reader = null;
-        	BufferedWriter out=null;
-        	fis = new FileInputStream("A1C-result.txt");
-        	reader = new BufferedReader(new InputStreamReader(fis));
-        	out = new BufferedWriter(new FileWriter("Tagged.txt"));
-        	System.out.println("Reading File line by line using BufferedReader");
-        	String line = reader.readLine();
-        	while(line != null){
-        		String tagged = tagger.tagString(line);
-        	
-        	out.write(tagged);
-        	out.newLine();
-        	
-        	line = reader.readLine();
-        	}
-        	 out.close();
-        	 System.out.println("Done");
-       
+            "taggers/bidirectional-distsim-wsj-0-18.tagger");
+        //reading file line by line in Java using BufferedReader
+        FileInputStream fis = null;
+        BufferedReader reader = null;
+        BufferedWriter out=null;
+        fis = new FileInputStream("A1C-result.txt");
+        reader = new BufferedReader(new InputStreamReader(fis));
+        out = new BufferedWriter(new FileWriter("Tagged.txt"));
+        System.out.println("Reading File line by line using BufferedReader");
+        String line = reader.readLine();
+        while(line != null){
+            String tagged = tagger.tagString(line);
+            out.write(tagged);
+            out.newLine();
+            
+            line = reader.readLine();
+        }
+        out.close();
+        System.out.println("Done");
     }
 }
